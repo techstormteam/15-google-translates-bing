15c9d4491f8e359232ff022f15d1d01fcd35e5f4
vinh.thien0301 <vinh.thien0301@gmail.com>
2015-02-12 11:11:49 +0700

    fix latest

--- GoogleBingTranslates/src/com/techstorm/Config.java
+++ GoogleBingTranslates/src/com/techstorm/Config.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Config {
 	public boolean useGoogle = false;
 	public boolean useBing = false;
+	public String inputCsv;
+	public String outputCsv;
+	public String accountsCsv;
+	public String columns;
 	public String fromLanguage;
 	public List<String> toLanguages;
 }
--- GoogleBingTranslates/src/com/techstorm/TranslatorApp.java
+++ GoogleBingTranslates/src/com/techstorm/TranslatorApp.java
@@ -1,10 +1,13 @@
 package com.techstorm;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import sample.util.SimpleCommandLineParser;
@@ -21,51 +24,27 @@ import com.translator.google.translator.OnlineGoogleTranslator.Language;
 
 public class TranslatorApp {
 	private static OnlineGoogleTranslator translator;
-	private static Translation translation;
+	private static List<Integer> columnIndexes;
 	
 	public static void main(String[] args) throws Exception {
-		SimpleCommandLineParser parser = new SimpleCommandLineParser(args);
-		
-		boolean help = parser.containsKey("help", "h");
-		String inputCsv = parser.getValue("inputcsv", "i");
-		String columns = parser.getValue("columns", "c");
-		String outputCsv = "output.csv";
-		String accountsCsv = "accounts.csv";
-		
 		Config config = readConfiguration();
 		
-		if (help || inputCsv == null || columns == null) {
-			printUsage();
-			System.exit(1);
-		}
+		Map<String,List<List<String>>> outputs = new HashMap<String,List<List<String>>>(); 
 		
-		List<Integer> columnIndexes = null;
-		try {
-			columnIndexes = new ArrayList<Integer>();
-			String[] columnArray = columns.split(",");
-			for (String column : columnArray) {
-				columnIndexes.add(Integer.parseInt(column));
-			}
-		} catch (NumberFormatException e) {
-			// nothing
-		}
-		
-		List<List<String>> csvContent = new ArrayList<List<String>>();
 		List<Account> googleAccounts = new ArrayList<Account>();
 		List<Account> bingAccounts = new ArrayList<Account>();
-		
-		if (columnIndexes == null) {
-			printUsage();
-			System.exit(1);
-		}
 		
 		// read csv file for each rows
 		CSV csv = CSV.separator(',') // delimiter of fields
 			.noQuote() 
 			.create(); // new instance is immutable
 		
-		csv.read(accountsCsv, new CSVReadProc() {
+		csv.read(config.accountsCsv, new CSVReadProc() {
 			public void procRow(int rowIndex, String... values) {
+				if (rowIndex == 0) {
+					return;
+				}
+				
 				Account account = new Account();
 				account.username = values[0];
 				account.password = values[1];
@@ -81,49 +60,101 @@ public class TranslatorApp {
 				
 			}
 		});
-		
-		csv.read(inputCsv, new CSVReadProc() {
-			public void procRow(int rowIndex, String... values) {
-				List<String> rowContent = new ArrayList<String>();
-				for (String value : values) {
-					rowContent.add(value);
-				}
-				csvContent.add(rowContent);
+		if (googleAccounts != null && !googleAccounts.isEmpty()) {
+			translator = OnlineGoogleTranslator.createInstance(googleAccounts.get(0).apiKey);
+		}
+		if (bingAccounts != null && !bingAccounts.isEmpty()) {
+			setAuthForBingTranslates(bingAccounts.get(0).clientId, bingAccounts.get(0).secret);
+		}
+				
+		columnIndexes = null;
+		try {
+			columnIndexes = new ArrayList<Integer>();
+			String[] columnArray = config.columns.split(",");
+			for (String column : columnArray) {
+				columnIndexes.add(Integer.parseInt(column));
 			}
-		});
-		
-		for (List<String> rowContent : csvContent) {
-			for (Integer columnIndex : columnIndexes) {
-				String value = rowContent.get(columnIndex);
-				String translatedText = "";
-				for (String toLanguage : config.toLanguages) {
-					if (config.useGoogle) {
-						translatedText = googleTranslates(value, config.fromLanguage, toLanguage);
-					} else if (config.useBing) {
-						translatedText = bingTranslates(value, config.fromLanguage, toLanguage);
-					}
-				}
-				rowContent.set(columnIndex, translatedText);
-			}
+		} catch (NumberFormatException e) {
+			// nothing
 		}
 		
-		csv.write(outputCsv, new CSVWriteProc() {
-			public void process(CSVWriter out) {
-				for (List<String> rowContent : csvContent) {
+		
+		
+		if (columnIndexes == null) {
+			System.exit(1);
+		}
+		
+		for (String toLanguage : config.toLanguages) {
+			List<List<String>> csvContent = new ArrayList<List<String>>();
+			outputs.put(toLanguage, csvContent);
+		}
+		
+		
+		
+		csv.read(config.inputCsv, new CSVReadProc() {
+			public void procRow(int rowIndex, String... values) {
+				for (String toLanguage : config.toLanguages) {
 					
-					if (rowContent != null && !rowContent.isEmpty()) {
-						StringBuilder rowString = new StringBuilder();
-						for (String value : rowContent) {
-							rowString.append(value);
-							rowString.append(",");
+					List<List<String>> csvContent = outputs.get(toLanguage);
+					List<String> rowContent = new ArrayList<String>();
+					for (int index = 0; index < values.length; index++) {
+						String value = values[index];
+						for (Integer columnIndex : columnIndexes) {
+							if (columnIndex - 1 == index) { 
+								
+									try {
+										if (config.useGoogle) {
+											value = googleTranslates(value, config.fromLanguage, toLanguage);
+										} else if (config.useBing) {
+											value = bingTranslates(value, config.fromLanguage, toLanguage);
+										}
+									} catch (Exception e) {
+										e.printStackTrace();
+									}
+								
+							}
 						}
-						rowString.deleteCharAt(rowString.length() - 1);
-						out.writeNext(rowString.toString());
+						rowContent.add(value);
 					}
-					
+					csvContent.add(rowContent);
 				}
 			}
 		});
+		
+		for (String languageKey : outputs.keySet()) {
+			
+			csv.write(getOutputCsvLanguage(config.outputCsv, languageKey), new CSVWriteProc() {
+				public void process(CSVWriter out) {
+					List<List<String>> csvContent = outputs.get(languageKey);
+					for (List<String> rowContent : csvContent) {
+						
+						if (rowContent != null && !rowContent.isEmpty()) {
+							StringBuilder rowString = new StringBuilder();
+							for (String value : rowContent) {
+								rowString.append(value);
+								rowString.append(",");
+							}
+							rowString.deleteCharAt(rowString.length() - 1);
+							out.writeNext(rowString.toString());
+						}
+						
+					}
+				}
+			});
+		}
+		
+	}
+	
+	private static String getOutputCsvLanguage(String outputOriginalFilename, String languageKey) {
+		StringBuilder result = new StringBuilder();
+		String[] parts = outputOriginalFilename.split("\\.");
+		if (parts != null && parts.length > 0) {
+			result.append(parts[0]);
+		}
+		result.append("_");
+		result.append(languageKey);
+		result.append(".csv");
+		return result.toString();
 	}
 	
 	private static Config readConfiguration() {
@@ -134,19 +165,30 @@ public class TranslatorApp {
 	 
 		try {
 	 
-			input = new FileInputStream("../config.properties");
+			input = new FileInputStream("config.properties");
 	 
 			// load a properties file
 			prop.load(input);
 	 
 			String google = prop.getProperty("google");
-			if (google.equals("0")) {
+			if (google.equalsIgnoreCase("YES")) {
 				result.useGoogle = true;
 			}
 			String bing = prop.getProperty("bing");
-			if (bing.equals("0")) {
+			if (bing.equalsIgnoreCase("YES")) {
 				result.useBing = true;
 			}
+			
+			String inputCsv = prop.getProperty("inputCsv");
+			result.inputCsv = inputCsv;
+			String outputCsv = prop.getProperty("outputCsv");
+			result.outputCsv = outputCsv;
+			String accountsCsv = prop.getProperty("accountsCsv");
+			result.accountsCsv = accountsCsv;
+			String columns = prop.getProperty("columns");
+			result.columns = columns;
+			
+			
 			String fromLang = prop.getProperty("fromLanguage");
 			result.fromLanguage = fromLang;
 			
@@ -202,10 +244,7 @@ public class TranslatorApp {
 	private static void printUsage() {
 		System.out.println("Usage: java -jar GoogleBingTranslates.jar"
 				+ " --inputcsv <CSV PATH>"
-				+ " --columns <TRANSLATES COLUMN INDEXES. e.i: '1,3,4'>"
-				+ " --fromlang <FROM LANGUAGE. e.i: 'TURKISH' or 'turkish' or 'tr'>"
-				+ " --tolang <TO LANGUAGE. e.i: 'ENGLISH' or 'english' or 'en'>"
-				+ " --use <TRANSLATE SOURCE. e.i: 'google' or 'bing'>");
+				+ " --columns <TRANSLATES COLUMN INDEXES. e.i: '1,3,4'>");
 	}
 	
 	private static String googleTranslates(String text, String fromLang, String toLang) throws Exception {
@@ -214,24 +253,20 @@ public class TranslatorApp {
         final Language SOURCE_LANGUAGE = getGLanguageObject(fromLang);
         final Language TARGET_LANGUAGE = getGLanguageObject(toLang);
 
-        // specific setting only for Google Translate API v2
-        final String API_KEY = "AIzaSyDx4EOP-SO8KM6pCXGOi9D7lv3a4X4S-6g";
-
-		translator = OnlineGoogleTranslator.createInstance(API_KEY);
-        translation = translator.translate(SOURCE_TEXT, SOURCE_LANGUAGE, TARGET_LANGUAGE);
-
+        Translation translation = translator.translate(SOURCE_TEXT, SOURCE_LANGUAGE, TARGET_LANGUAGE);
         return translation.getTranslatedText();
 	}
 	
 	private static String bingTranslates(String text, String fromLang, String toLang) throws Exception {
-		// Set your Windows Azure Marketplace client info - See http://msdn.microsoft.com/en-us/library/hh454950.aspx
-	    Translate.setClientId("BingTranslatesAuto");
-	    Translate.setClientSecret("2mloy/ylg8VapooWqoULMdxCHrzRHJ0wfRNbh55g/Sk=");
-
 	    String translatedText = Translate.execute(text, getBLanguageObject(fromLang), getBLanguageObject(toLang));
+	    
 	    return translatedText;
 	}
 	
+	private static void setAuthForBingTranslates(String clientId, String secret) {
+		Translate.setClientId(clientId);
+	    Translate.setClientSecret(secret);
+	}
 	
 }
 
